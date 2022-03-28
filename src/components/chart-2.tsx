4b174e15e7eb1d43fab14a3e64f94445eba16ea1
MadarainCong <huangcong0307@163.com>
2022-03-28 15:10:43 +0800

    添加渐变，出了点bug

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -10,37 +10,67 @@ export const Chart2 = () =>{
   useEffect(() => {
   var myChart = echarts.init(divRef.current);
   myChart.setOption(createEchartsOptions({
-    ... baseEchartOptions,
-    grid:{
-      left:'20%',
-    },
     xAxis: {
-        type: 'value',
-        boundaryGap: [0, 0.01]
+      type: 'value',
+      boundaryGap: [0, 0.01]
     },
     yAxis: {
-        type: 'category',
-        data: ['Brazil', 'Indonesia', 'USA', 'India', 'China', 'World']
+      axisTick: {show: false},
+      type: 'category',
+      data: ['城关区公安局', '七里河区公安局', '西固区公安局', '安宁区公安局', '红古区公安局',
+          '永登县公安局', '皋兰县公安局', '榆中县公安局', '新区公安局'],
+      axisLabel: {
+        formatter(val) {
+          return val.replace('公安局', '\n公安局');
+        }
+      }          
     },
     series: [
         {
           name: '破案排名1',
           type: 'bar',
-          data: [18203, 23489, 29034, 104970, 131744, 630230]
+          data: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+          itemStyle: {
+            normal: {
+              color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                offset: 0,
+                color: '#2034f9'
+              }, {
+                offset: 1,
+                color: '#04a1ff'
+              }]),
+            }
+          }
         },
         {
-          name: '破案排名2',
+          name: '2012年',
           type: 'bar',
-          data: [19325, 23438, 31000, 121594, 134141, 681807]
+          data: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+          itemStyle: {
+            normal: {
+              color: new echarts.graphic.LinearGradient(0, 0, 1, 0, [{
+                offset: 0,
+                color: '#b92ae8'
+              }, {
+                offset: 1,
+                color: '#6773e7'
+              }]),
+            }
+          }
         }
     ]
   }));
   }, []);
 
   return (
-    <div className="bordered 管辖统计">
+    <div className="bordered 破获排名">
         <h2>案件破获排名</h2>
-        <div ref={divRef} className="chart"></div>
+        <div ref={divRef} className="chart">
+          <div className="legend">
+            <span className="first">破案排名1</span>
+            <span className="second">破案排名2</span>
+          </div>
+        </div>
     </div>
   )
 }
